b44d3acdc4b795e0bff281d88cc6d091c503eb07
danielpopp <Daniel.Popp@sbs-herzogenaurach.de>
2025-09-23 11:54:47 +0200

    Commit

--- CentralWidget.py
+++ CentralWidget.py
@@ -1,8 +1,34 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QLineEdit, QGridLayout, QTextEdit, QTextBrowser, QLabel, QPushButton
 
 
 class CentralWidget(QWidget):
     def __init__(self, parent=None):
         super(CentralWidget, self).__init__(parent)
 
-        # Lets go...
+        layout = QGridLayout()
+
+        self.__benutzername = QLineEdit()
+        self.__kennwort = QLineEdit()
+        self.__token = QLineEdit()
+
+        self.__login = QPushButton("Login")
+        self.__abbruch = QPushButton("Abbruch")
+
+        self.__text_browser = QTextBrowser()
+
+        layout.addWidget(QLabel("Benutzername:"), 0, 0, 1, 2)
+        layout.addWidget(self.__benutzername, 1, 0, 1, 2)
+
+        layout.addWidget(QLabel("Kennwort:"), 2, 0, 1, 2)
+        layout.addWidget(self.__kennwort, 3, 0, 1, 2)
+
+        layout.addWidget(QLabel("Token:"), 4, 0, 1, 2)
+        layout.addWidget(self.__token, 5, 0, 1, 2)
+
+        layout.addWidget(self.__login, 6, 0)
+        layout.addWidget(self.__abbruch, 6, 1)
+
+        layout.addWidget(QLabel("Ergebnis:"), 0, 2)
+        layout.addWidget(self.__text_browser, 1, 2, 6, 1)
+
+        self.setLayout(layout)
